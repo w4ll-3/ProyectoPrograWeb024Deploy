17018c4b73e37fcc3fe2be8c246d19cc533cd101
Wellington Cua <wjcua@correo.url.edu.gt>
2024-04-18 00:21:57 -0600

    se crearon los componentes about y experience, se agrego la data con el archivo experience.js, la imagen de perfil, las animaciones de ayuda (helpers) animation-lottie.jsx y glow-card.jsx, y se agrego la imagen en formato json para la seccion de experiencias

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import AboutSection from "./components/homepage/about";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
+      <Experience />
+
     </>
   );
 }
---
+++ utils/data/experience.js
@@ -0,0 +1,32 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Project Management Assistant",
+    company: "Oowlish Technologies",
+    duration: "(Ene 2024 - Present)",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer",
+    company: "Oowlish Technologies",
+    duration: "(Abr 2023 - Dic 2023)",
+  },
+  {
+    id: 3,
+    title: "Odoo Developer",
+    company: "Abacus Intelligence",
+    duration: "(Ene 2022 - Feb 2023)",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    company: "Prometeo TI",
+    duration: "(Abr 2021 - Dec 2021)",
+  },
+  {
+    id: 5,
+    title: "Frontend Developer",
+    company: "Totaldoc",
+    duration: "(Ene 2021 - Mar 2021)",
+  },
+];
