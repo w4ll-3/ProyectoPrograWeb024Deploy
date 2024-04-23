a4f03fde33f066510f3b5968f43085eaebce7d95
Wellington Cua <wjcua@correo.url.edu.gt>
2024-04-23 00:11:44 -0600

    utils/skill-image.js funcion para retornar la imagen segun la data de skills.js modificada para usar un hash table; components/homepage/skills: componente skills creado, animacion de carrusel usada para mostrar los skills; public/svg: archivos .svg agregados: bootstrap, markdown y sass; utils/data: archivos skills.js creado con la informacion necesaria para cargar las imagenes en el carrusel

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import AboutSection from "./components/homepage/about";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
+import Skills from "./components/homepage/skills";
 
 export default async function Home() {
   return (
@@ -8,7 +9,7 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-
+      <Skills />
     </>
   );
 }
--- utils/skill-image.js
+++ utils/skill-image.js
@@ -1,6 +1,60 @@
-import { skills } from "../public/svg/skills";
+import angular from "../public/svg/skills/angular.svg";
+import aws from "../public/svg/skills/aws.svg";
+import azure from "../public/svg/skills/azure.svg";
+import bootstrap from "../public/svg/skills/bootstrap.svg";
+import css from "../public/svg/skills/css.svg";
+import figma from "../public/svg/skills/figma.svg";
+import firebase from "../public/svg/skills/firebase.svg";
+import gcp from "../public/svg/skills/gcp.svg";
+import git from "../public/svg/skills/git.svg";
+import graphql from "../public/svg/skills/graphql.svg";
+import html from "../public/svg/skills/html.svg";
+import ionic from "../public/svg/skills/ionic.svg";
+import javascript from "../public/svg/skills/javascript.svg";
+import markdown from "../public/svg/skills/markdown.svg";
+import materialui from "../public/svg/skills/materialui.svg";
+import microsoftoffice from "../public/svg/skills/microsoftoffice.svg";
+import mongoDB from "../public/svg/skills/mongoDB.svg";
+import mysql from "../public/svg/skills/mysql.svg";
+import nextJS from "../public/svg/skills/nextJS.svg";
+import postgresql from "../public/svg/skills/postgresql.svg";
+import python from "../public/svg/skills/python.svg";
+import react from "../public/svg/skills/react.svg";
+import sass from "../public/svg/skills/sass.svg";
+import selenium from "../public/svg/skills/selenium.svg";
+import tailwind from "../public/svg/skills/tailwind.svg";
+import typescript from "../public/svg/skills/typescript.svg";
 
 export const skillsImage = (skill) => {
-  const skillImage = skills.find((s) => s.name === skill);
-  return skillImage ? skillImage.image : null;
+  const skillID = skill.toLowerCase();
+  const skillMap = {
+    angular: angular,
+    aws: aws,
+    azure: azure,
+    bootstrap: bootstrap,
+    css: css,
+    figma: figma,
+    firebase: firebase,
+    gcp: gcp,
+    git: git,
+    graphql: graphql,
+    html: html,
+    ionic: ionic,
+    javascript: javascript,
+    markdown: markdown,
+    materialui: materialui,
+    "ms office": microsoftoffice,
+    mongodb: mongoDB,
+    mysql: mysql,
+    nextjs: nextJS,
+    postgresql: postgresql,
+    python: python,
+    react: react,
+    sass: sass,
+    selenium: selenium,
+    tailwind: tailwind,
+    typescript: typescript,
+  };
+
+  return skillMap[skillID];
 };
