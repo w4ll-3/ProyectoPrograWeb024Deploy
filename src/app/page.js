36fb8278e4a1044ec77b8e60d5d1612db8266c17
Wellington Cua <wjcua@correo.url.edu.gt>
2024-05-15 18:23:54 -0600

    funcion para traer a los posts de la pagina dev.to

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { personalData } from "../../utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
@@ -5,8 +6,28 @@ import Skills from "./components/homepage/skills";
 import Projects from "./components/homepage/projects";
 import Education from "./components/homepage/education";
 
+async function getData() {
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.devUsername}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+
+  const filtered = data
+    .filter((item) => item?.cover_image)
+    .sort(() => Math.random() - 0.5);
+
+  return filtered;
+}
 
 export default async function Home() {
+  const blogs = await getData();
+  console.log(blogs)
+
   return (
     <>
       <HeroSection />
