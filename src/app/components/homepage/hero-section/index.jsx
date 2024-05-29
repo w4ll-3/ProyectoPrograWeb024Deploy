a29f2e103b3cd9dd39fb92e4d1d5998540002b57
Wellington Cua <wjcua@correo.url.edu.gt>
2024-05-28 21:08:29 -0600

    modificacion de links a redes sociales, se elimino twitter y leetcode por poco uso en el componente hero-section y en contacto

--- src/app/components/homepage/hero-section/index.jsx
+++ src/app/components/homepage/hero-section/index.jsx
@@ -4,10 +4,10 @@ import { personalData } from "../../../../../utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
-import { SiLeetcode } from "react-icons/si";
+import { SiStackoverflow } from "react-icons/si";
 
 function HeroSection() {
   return (
@@ -55,18 +55,11 @@ function HeroSection() {
               <FaFacebook size={30} />
             </Link>
             <Link
-              href={personalData.leetcode}
+              href={personalData.stackOverflow}
               target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
-              <SiLeetcode size={30} />
-            </Link>
-            <Link
-              href={personalData.twitter}
-              target='_blank'
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <FaTwitterSquare size={30} />
+              <SiStackoverflow size={30} />
             </Link>
           </div>
 
